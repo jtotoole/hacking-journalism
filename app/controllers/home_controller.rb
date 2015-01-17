c4ffe8937eb92597a1058e7bb4d54e4cd2e268d1
Miles Stanfield <miles.stanfield@turner.com>
2015-01-17 17:15:27 -0500

    added delete capability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,12 +10,21 @@ class HomeController < ApplicationController
     unless no_param?(text) || no_param?(time) || no_param?(position_x) || no_param?(position_y)
       Comment.create!(text: text, position_x: position_x, position_y: position_y, time: time)
     end
+    render "success"
   end
 
   def get_comments
     @comments = Comment.all
     respond_to do |format|
       format.json { render "get_comment.json"}
+    end
+  end
+
+  def delete_comment
+    id = params[:id]
+    unless no_param?(id)
+      Comment.find(id).destroy
+      render "success"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/submit_comment", to: "home#submit_comment"
-
   get "/get_comments", to: "home#get_comments", defaults: { format: 'json' }
+  get "/delete_comment", to: "home#delete_comment"
 end

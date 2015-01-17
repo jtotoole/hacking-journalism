9d9a57030aee06c836b0cf3781499871f0a43393
Miles Stanfield <miles.stanfield@turner.com>
2015-01-17 17:52:07 -0500

    removed default json to make sure it does jsonp? we will see

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/submit_comment", to: "home#submit_comment"
-  get "/get_comments", to: "home#get_comments", defaults: { format: 'json' }, as: "get_comments"
+  get "/get_comments", to: "home#get_comments", as: "get_comments"
   get "/delete_comment", to: "home#delete_comment"
 end

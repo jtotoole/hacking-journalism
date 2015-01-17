21d4c66e3166c430b30c7dd13e99ff676b06fa19
Miles Stanfield <miles.stanfield@turner.com>
2015-01-17 18:20:17 -0500

    removing verify auth token from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, if: :json_request?
+
+  def json_request?
+    request.format.json?
+  end
+
 
   def authenticate
     if Rails.env.development? || Rails.env.test?

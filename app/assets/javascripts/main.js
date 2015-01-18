8678b2076183ca2fbd5cb188e2c8a0e7eef284e6
Emily Chow <emily.sy.chow@gmail.com>
2015-01-18 15:03:49 -0500

    Merged in video

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 var COMMENT_TIME_ON_SCREEN_SECONDS = 2;
 var TEXT_MAX = 40;
+var OVERLAY_VIDEOS = false;
 
 ZiggeoApi.token = "f98b6bd1fb6223951a4f344be3ad5753";
 
@@ -79,7 +80,7 @@ function addEventListeners(){
 $(document).ready(function() {
 	addEventListeners();
 
-	videojs("myvideo").ready(function(){
+	videojs("myvideo").ready(function() {
 		var myPlayer = this;
 		var play_time = 0;
 
@@ -96,77 +97,71 @@ $(document).ready(function() {
 			});
 		});
     
-  //   ZiggeoApi.Events.on("submitted", function(data) {
-		// 	console.log(data);
+  		ZiggeoApi.Events.on("submitted", function(data) {
+			console.log(data);
 
-		// 	$("#recorder").hide();
+		 	$("#recorder").hide();
 
-		// 	recorder.reset();
+		 	recorder.reset();
 
-		// 	var position_percentages = {
-		// 		left: 0,
-		// 		top: 0
-		// 	};
+		 	var position_percentages = {
+		 		left: 0,
+		 		top: 0
+		 	};
 
-		// 	var play_time = myPlayer.currentTime();
+		 	var play_time = myPlayer.currentTime();
 
-		// 	$.get("//mst4k.herokuapp.com/submit_comment", {
-		// 		auth: "hackingyovids",
-		// 		time: play_time,
-		// 		text: data.video.token,
-		// 		user: user_name,
-		// 		position_x: position_percentages.left,
-		// 		position_y: position_percentages.top,
-		// 		kind: "video"
-		// 	});
+		 	$.get("//mst4k.herokuapp.com/submit_comment", {
+		 		auth: "hackingyovids",
+		 		time: play_time,
+		 		text: data.video.token,
+		 		user: user_name,
+		 		position_x: position_percentages.left,
+		 		position_y: position_percentages.top,
+		 		kind: "video"
+		 	});
 
-		// 	$("#close_recorder").hide();
-		// });
+		 	$("#close_recorder").hide();
+		 });
 
-		// ZiggeoApi.Events.on("stop", function (data) {
-		// 	$("#"+data.video.token).remove();
+		 ZiggeoApi.Events.on("stop", function (data) {
+		 	$("#"+data.video.token).remove();
 
-		// 	if(OVERLAY_VIDEOS == false ) {
-		// 		myPlayer.play();
-		// 	}
-		// });
+		 	if(OVERLAY_VIDEOS == false ) {
+		 		myPlayer.play();
+		 	}
+		 });
     
-  //   $("#close_recorder").click(function(e){
-		// 		$("#close_recorder").hide();
-		// 		$("#recorder").hide();
-		// 		$("#record_button").show();
+  		 $("#close_recorder").click(function(e){
+			$("#close_recorder").hide();
+			$("#recorder").hide();
+			$("#record_button").show();
 
-		// 		myPlayer.play();
-		// });
+			myPlayer.play();
+		 });
 
-		// myPlayer.on("timeupdate", function (){
-		// 	var currentTime = myPlayer.currentTime();
+		 myPlayer.on("timeupdate", function (){
+		 	var currentTime = myPlayer.currentTime();
 
-		// 	for(var i = 0; i < comments.length; i++) {
+		 	for(var i = 0; i < comments.length; i++) {
 
-		// 		var commentTime = parseFloat(comments[i].time);
+		 		var commentTime = parseFloat(comments[i].time);
 
-		// 		if(currentTime >= commentTime - 1.5 && currentTime <= commentTime && comments[i].preloaded != true) {
-		// 			if(comments[i].kind == "video") {
-		// 				el = $("<div class='player' id='" + comments[i].text + "''></div>");
-		// 				$("body").append(el);
-		// 				player = ZiggeoApi.Embed.embed("#" + comments[i].text, { width: 320, height: 240, video: comments[i].text });
-		// 				comments[i].element = el;
-		// 				comments[i].player = player;
-		// 				comments[i].preloaded = true;
+		 		if(currentTime >= commentTime - 1.5 && currentTime <= commentTime && comments[i].preloaded != true) {
+		 			if(comments[i].kind == "video") {
+		 				el = $("<div class='player' id='" + comments[i].text + "''></div>");
+		 				$("body").append(el);
+		 				player = ZiggeoApi.Embed.embed("#" + comments[i].text, { width: 320, height: 240, video: comments[i].text });
+		 				comments[i].element = el;
+		 				comments[i].player = player;
+		 				comments[i].preloaded = true;
 
-		// 			}
-		// 		}
+		 			}
+		 		}
 
-		// 		if(currentTime >= commentTime && currentTime <= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS && comments[i].visible != true) {
-		// 			comments[i].visible = true;
+		 		if(currentTime >= commentTime && currentTime <= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS && comments[i].visible != true) {
+		 			comments[i].visible = true;
 
-<<<<<<< HEAD
-		// 			if(comments[i].kind == "text") {
-		// 				var el = $("<div class='text-comment'>" + comments[i].user + ": " + comments[i].text + "</div>");
-		// 				var video = $("#myvideo_html5_api");
-						
-		// 				var video_position = video.position();
 					if(comments[i].kind == "text") {
 						var video = $("#myvideo_html5_api");
 						var video_position = video.position();
@@ -174,43 +169,43 @@ $(document).ready(function() {
         
 					        var video_position = video.position();
 
-		// 				var video_height = video.height();
-		// 				var video_width  = video.width();
+		 				var video_height = video.height();
+		 				var video_width  = video.width();
 
 
-		// 				var comment_left = parseFloat(comments[i].position_x) * video_width+video_position.left;
-		// 				var comment_top  = parseFloat(comments[i].position_y) * video_height + video_position.top;
+		 				var comment_left = parseFloat(comments[i].position_x) * video_width+video_position.left;
+		 				var comment_top  = parseFloat(comments[i].position_y) * video_height + video_position.top;
 
-		// 				el.css({
-		// 					top: comment_top,
-		// 					left: comment_left
-		// 				});
+		 				el.css({
+		 					top: comment_top,
+		 					left: comment_left
+		 				});
 
-		// 				$("body").append(el);
+		 				$("body").append(el);
 
-		// 				comments[i].element = el;
-		// 			}
-		// 			else if(comments[i].kind == "video") {
-		// 				if(comments[i].preloaded == true) {
-		// 					comments[i].element.css({ visibility: 'visible' });
-		// 					comments[i].player.play();
+		 				comments[i].element = el;
+		 			}
+		 			else if(comments[i].kind == "video") {
+		 				if(comments[i].preloaded == true) {
+		 					comments[i].element.css({ visibility: 'visible' });
+		 					comments[i].player.play();
 
-		// 					if(OVERLAY_VIDEOS == false) {
-		// 						myPlayer.pause();
-		// 					}
-		// 				}
-		// 			}
-		// 		}
+		 					if(OVERLAY_VIDEOS == false) {
+		 						myPlayer.pause();
+		 					}
+		 				}
+		 			}
+		 		}
 
-		// 		if(comments[i].visible == true && (currentTime <= commentTime || currentTime >= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS)) {
-		// 			if(comments[i].kind == "text") {
-		// 				comments[i].element.remove();
+		 		if(comments[i].visible == true && (currentTime <= commentTime || currentTime >= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS)) {
+		 			if(comments[i].kind == "text") {
+		 				comments[i].element.remove();
 
-		// 				comments[i].visible = false;
-		// 			}
-		// 		}
-		// 	}
-		// });
+		 				comments[i].visible = false;
+		 			}
+		 		}
+		 	}
+		 });
 
 		$("#myvideo_html5_api").click(function(e) {
 			console.log("Video clicked", e);
@@ -265,9 +260,10 @@ $(document).ready(function() {
 				}
 
 
-        // $(this).hide();
-        //hide input options
-        $("#input-options").hide();
+				// $(this).hide();
+				//hide input options
+				$("#input-options").hide();
+
 				$(this).val("");
 
 				$.get("https://mst4k.herokuapp.com/submit_comment", {
@@ -281,7 +277,7 @@ $(document).ready(function() {
 
 				myPlayer.play();
 
-			}else {
+			} else {
 				if ($(this).val().length > TEXT_MAX){
 					return false;
 				}

25ee83031f888beed32aded7908f80b6ccb70a11
Herb Susmann <herb@MACs-MacBook-Pro.local>
2015-01-18 13:33:15 -0500

    Added video recording

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,8 @@
 var COMMENT_TIME_ON_SCREEN_SECONDS = 2;
 var TEXT_MAX = 40;
 
+var OVERLAY_VIDEOS = true;
+
 comments = [];
 
 function load_initial_comments() {
@@ -27,7 +29,8 @@ load_initial_comments();
 	}
 ];*/
 
-
+var recorder;
+var player;
 
 
 $(document).ready(function() {
@@ -35,9 +38,46 @@ $(document).ready(function() {
 		var myPlayer = this;
 		var play_time = 0;
 
+		recorder = ZiggeoApi.Embed.embed("#recorder", {width: "320", height: "240", countdown: 0 });
+
 		var user_name = "James";
 		//var user_name = window.prompt("Please enter your name.");
 
+		ZiggeoApi.Events.on("submitted", function(data) {
+			console.log(data);
+
+			$("#recorder").hide();
+
+			recorder.reset();
+
+			var position_percentages = {
+				left: 0,
+				top: 0
+			};
+
+			var play_time = myPlayer.currentTime();
+
+			$.get("//mst4k.herokuapp.com/submit_comment", {
+				auth: "hackingyovids",
+				time: play_time,
+				text: data.video.token,
+				user: user_name,
+				position_x: position_percentages.left,
+				position_y: position_percentages.top,
+				kind: "video"
+			});
+
+			$("#close_recorder").hide();
+		});
+
+		ZiggeoApi.Events.on("stop", function (data) {
+			$("#"+data.video.token).remove();
+
+			if(OVERLAY_VIDEOS == false ) {
+				myPlayer.play();
+			}
+		});
+		$("recorder").css({width: "320px", height:"240px"});
 
 		myPlayer.on("timeupdate", function (){
 			var currentTime = myPlayer.currentTime();
@@ -46,37 +86,79 @@ $(document).ready(function() {
 
 				var commentTime = parseFloat(comments[i].time);
 
+				if(currentTime >= commentTime - 1.5 && currentTime <= commentTime && comments[i].preloaded != true) {
+					if(comments[i].kind == "video") {
+						el = $("<div class='player' id='" + comments[i].text + "''></div>");
+						$("body").append(el);
+						player = ZiggeoApi.Embed.embed("#" + comments[i].text, { width: 320, height: 240, video: comments[i].text });
+						comments[i].element = el;
+						comments[i].player = player;
+						comments[i].preloaded = true;
+
+					}
+				}
+
 				if(currentTime >= commentTime && currentTime <= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS && comments[i].visible != true) {
 					comments[i].visible = true;
 
-					var el = $("<div class='text-comment'>" + comments[i].user + ": " + comments[i].text + "</div>");
-					var video = $("#myvideo_html5_api");
-					
-					var video_position = video.position();
+					if(comments[i].kind == "text") {
+						var el = $("<div class='text-comment'>" + comments[i].user + ": " + comments[i].text + "</div>");
+						var video = $("#myvideo_html5_api");
+						
+						var video_position = video.position();
 
-					var video_height = video.height();
-					var video_width  = video.width();
+						var video_height = video.height();
+						var video_width  = video.width();
 
 
-					var comment_left = parseFloat(comments[i].position_x) * video_width+video_position.left;
-					var comment_top  = parseFloat(comments[i].position_y) * video_height + video_position.top;
+						var comment_left = parseFloat(comments[i].position_x) * video_width+video_position.left;
+						var comment_top  = parseFloat(comments[i].position_y) * video_height + video_position.top;
 
-					el.css({
-						top: comment_top,
-						left: comment_left
-					});
+						el.css({
+							top: comment_top,
+							left: comment_left
+						});
 
-					$("body").append(el);
+						$("body").append(el);
 
-					comments[i].element = el;
+						comments[i].element = el;
+					}
+					else if(comments[i].kind == "video") {
+						if(comments[i].preloaded == true) {
+							comments[i].element.css({ visibility: 'visible' });
+							comments[i].player.play();
+
+							if(OVERLAY_VIDEOS == false) {
+								myPlayer.pause();
+							}
+						}
+					}
 				}
-
 
 				if(comments[i].visible == true && (currentTime <= commentTime || currentTime >= commentTime  + COMMENT_TIME_ON_SCREEN_SECONDS)) {
-					comments[i].element.remove();
-					comments[i].visible = false;
+					if(comments[i].kind == "text") {
+						comments[i].element.remove();
+
+						comments[i].visible = false;
+					}
 				}
 			}
+		});
+
+		$("#record_button").click(function(e){
+			$("#recorder").show();
+			$("#recorder").css({width: "320px", height: "240px"});
+			$("#close_recorder").show();
+			$(this).hide();
+			myPlayer.pause();
+		});
+
+		$("#close_recorder").click(function(e){
+				$("#close_recorder").hide();
+				$("#recorder").hide();
+				$("#record_button").show();
+
+				myPlayer.play();
 		});
 
 		$("#myvideo_html5_api").click(function(e) {
@@ -134,13 +216,14 @@ $(document).ready(function() {
 				$(this).hide();
 				$(this).val("");
 
-				$.get("https://mst4k.herokuapp.com/submit_comment", {
+				$.get("//mst4k.herokuapp.com/submit_comment", {
 					auth: "hackingyovids",
 					time: play_time,
 					text: text,
 					user: user_name,
 					position_x: position_percentages.left,
-					position_y: position_percentages.top
+					position_y: position_percentages.top,
+					kind: "text"
 				});
 				
 				myPlayer.play();
